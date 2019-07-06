d99f13726136ae46fb1f67b7c6f3f15fbf00ec3f
Ivan Kravtsov <cold67@mail.ru>
2019-07-06 21:20:23 +0300

    1.2.2-name-standart commit

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -8,6 +8,7 @@ $fullName = $lastName . ' ' . $firstName . ' ' . $middleName;
 $fio = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($middleName, 0, 1);
 $surnameAndInitials = $lastName . ' ' . mb_substr($firstName, 0, 1) . '.' . mb_substr($middleName, 0, 1) . '.';
 
+
 ?>
 
 <!DOCTYPE html>
