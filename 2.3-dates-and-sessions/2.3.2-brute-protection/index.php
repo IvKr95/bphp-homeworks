cdb6810cacf136b2dca0af9f4539c195da1d249e
Ivan Kravtsov <cold67@mail.ru>
2019-07-27 14:13:33 +0300

    bruteforce first commit

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -1,6 +1,49 @@
 <?php
+
+session_start();
+
 $users = [
     'admin' => 'admin1234',
     'randomUser' => 'somePassword',
     'janitor' => 'nimbus2000'
 ];
+
+if(isset($_POST['submit'])) {
+
+    if (!isset($_SESSION['counter'])) {
+        $_SESSION['counter'] = 0;
+    };    
+
+    $login = trim($_POST['login']);
+    $pass = trim($_POST['password']);
+
+    if(array_key_exists($login, $users) && $users[$login] === $pass) {
+
+        echo 'Добро пожаловать!';
+        
+    } else {
+
+        $_SESSION['counter'] += 1;
+
+        setcookie('error[' . $_SESSION['counter'] . ']', time(), time()+3600, '/', 'ivkr95.000webhostapp.com');
+
+        if (isset($_COOKIE['error'])) {
+
+            if ((time() - $_COOKIE['error'][$_SESSION['counter'] - 1]) < 5 || (time() - $_COOKIE['error'][$_SESSION['counter'] - 2]) < 60) {
+
+                $dh = fopen($login, 'at');
+                $content = date('d.m.Y h:i:s', time());
+                fwrite($dh, $content . PHP_EOL);
+                fclose($dh);
+
+                exit('Слишком часто вводите пароль. Попробуйте еще раз через минуту.');
+
+            } else {
+                echo 'Неверно введены данные';
+            };
+        } else {
+            echo 'Неверно введены данные';
+        };
+    };
+};
+ 
